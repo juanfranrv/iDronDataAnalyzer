aaec4296e75b0e94166ba7c02db9bbd3154454ba
juanfranrv <juanfranrv@correo.ugr.es>
2015-09-30 02:14:18 +0200

    Borrando archivos sobrantes

--- src/server.py
+++ src/server.py
@@ -13,26 +13,90 @@ import urllib
 import sys
 import subprocess
 
-#Clase que devuelve el login..
+#Clase principal
 
-class mostrarLogin(webapp2.RequestHandler):
+class MainPage(webapp2.RequestHandler):
     
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        template_values={}
-        template = JINJA_ENVIRONMENT.get_template('template/login.html')
-        self.response.write(template.render(template_values))
+        
+        if self.request.cookies.get("username"):
+            
+            user=self.request.cookies.get("username")
+            self.response.headers['Content-Type'] = 'text/html'
+            template_values={'sesion':user}
+            template = JINJA_ENVIRONMENT.get_template('template/index.html')
+            self.response.write(template.render(template_values))
+            
+        else:
+            
+            self.response.headers['Content-Type'] = 'text/html'
+            template_values={}
+            template = JINJA_ENVIRONMENT.get_template('template/login.html')
+            self.response.write(template.render(template_values))
+            
+#Clase para gestionar el logueo del usuario.
+
+class Login(webapp2.RequestHandler):
+    
+    def get(self):
+            
+            self.response.headers['Content-Type'] = 'text/html'
+            template_values={}
+            template = JINJA_ENVIRONMENT.get_template('template/login.html')
+            self.response.write(template.render(template_values))
+            
+    #Implementa el login de usuarios guardando la sesión en una cookie.
+    
+    def post(self):
+        
+        usu=self.request.get('user')
+        result=model.Usuario.query(model.Usuario.usuario==usu)
+        usur=result.get()
+        
+        if usur is not None:
+            
+            usur=result.get()
+            pas=self.request.get('pass')
+            
+            if usur.password==pas:
+                
+                self.response.headers.add_header('Set-Cookie',"username="+str(usur.usuario))
+        
+                template_values={'sesion':usur.usuario}
+                template = JINJA_ENVIRONMENT.get_template('template/index.html')
+                self.response.write(template.render(template_values))                
+                
+            else:
+        
+                template_values={'mensaje':'Password incorrecta.'}
+                template = JINJA_ENVIRONMENT.get_template('template/login.html')
+                self.response.write(template.render(template_values))
+        else:
+        
+            template_values={'mensaje':'Usuario incorrecto.'}
+            template = JINJA_ENVIRONMENT.get_template('template/login.html')
+            self.response.write(template.render(template_values))
+            
+# Clase que cierra la sesión del usuario.
+
+class cerrar_sesion(webapp2.RequestHandler):
+    
+    def get(self):
+        
+        self.response.headers.add_header("Set-Cookie", "username=; Expires=Thu, 01-Jan-1970 00:00:00 GMT")
+        self.redirect('/login')
 
 #Clase que devuelve un formulario de registro.
 
 class formRegistro(webapp2.RequestHandler):
     
     def get(self):
+        
         self.response.headers['Content-Type'] = 'text/html'
         template = JINJA_ENVIRONMENT.get_template('template/registro.html')
         self.response.write(template.render(message=""))
 
-     #Método que registra al usuario si pasa las restricciones.
+    #Método que registra al usuario si pasa las restricciones.
 
     def post(self):
         
@@ -60,11 +124,12 @@ class formRegistro(webapp2.RequestHandler):
             template = JINJA_ENVIRONMENT.get_template('template/registro.html')
             self.response.write(template.render(message="El nombre de usuario se encuentra en uso"))
             
-                
 # Urls de la aplicación con sus clases asociadas.
 
-urls = [('/',mostrarLogin),
+urls = [('/', MainPage),
+        ('/login', Login),
         ('/formRegistro',formRegistro),
+        ('/logout', cerrar_sesion),
         #('/.*', ErrorPage)
        ]
 
