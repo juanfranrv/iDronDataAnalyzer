c61354f84f22fd192c46ccf22d9365ed7ec64761
juanfranrv <juanfran_983@hotmail.com>
2016-02-23 13:40:18 +0100

    Añadiendo inicializacion de submodulo al despliegue de Azure

--- despliegue_azure.sh
+++ despliegue_azure.sh
@@ -10,6 +10,12 @@ else
 
 	APPENGINE_SERVER="GoogleAppEngineSDK/dev_appserver.py"
 
+	#Iniciamos los módulos de GAE
+
+	git submodule init && \
+	git submodule sync && \
+	git submodule update
+
 	# Lanzar aplicación (con autoconfirmación)
 
 	echo y | python $APPENGINE_SERVER  src --host=0.0.0.0 --port=80 --admin_port=8080 --storage_path=database &
